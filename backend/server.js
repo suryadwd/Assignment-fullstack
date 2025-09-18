e868cefe39f49dbc0d79ba52127b20b6d31cedb8
suryadwd <surajdwivedi644@gmail.com>
2025-09-19 00:02:34 +0530

    domain saved

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(cookieParser());
 
 
 app.use(cors({
-  origin: ["http://localhost:5173", "https://assignment-fullstack-ns9n.vercel.app", "https://assignment-fullstack-ucke.vercel.app"],
+  origin: ["http://localhost:5173", "https://assignment-fullstack-ns9n.vercel.app", "https://assignment-fullstack-ucke.vercel.app","https://assignment.devsurya.space/"],
   credentials: true,
 }));
 
