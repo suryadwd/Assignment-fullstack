1d58ccb6b20734c7023b1641be6192c94f50614a
suryadwd <surajdwivedi644@gmail.com>
2025-09-18 23:02:06 +0530

    added

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -102,7 +102,7 @@ export const login = async (req, res) => {
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
-    return res.status(200).json({ success: true, message: 'Login successful' });
+    return res.status(200).json({ success: true, message: 'Login successful', token : token});
   } catch (e) {
     console.error('Login error:', e);
     res.status(500).json({ error: 'Login failed' });
